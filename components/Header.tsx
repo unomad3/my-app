6dfccd143bd31db893b3873349fb990f6ff902e5
Ulziiorshikh Batdorj <ulzii.batdorj@nih.gov-at-846214067917>
2025-06-04 11:41:05 -0400

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,26 +4,31 @@ export default function Header() {
   return (
     <header className="border-b">
       <div className="max-w-7xl mx-auto px-4">
-        <nav className="hidden md:flex">
-          <Link
-            href="/products"
-            className="border-l text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
-          >
-            Products
-          </Link>
-          <Link
-            href="/categories"
-            className="border-x text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
-          >
-            Categories
-          </Link>
-          <Link
-            href="/about"
-            className="border-r text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
-          >
-            About
-          </Link>
-        </nav>
+        <div className="flex">
+          <div className="my-auto px-4">
+            <Link href="/">Logo</Link>
+          </div>
+          <nav className="hidden md:flex">
+            <Link
+              href="/products"
+              className="border-l text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
+            >
+              Products
+            </Link>
+            <Link
+              href="/categories"
+              className="border-x text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
+            >
+              Categories
+            </Link>
+            <Link
+              href="/about"
+              className="border-r text-gray-700 hover:text-gray-900 px-3 py-2 text-sm font-medium"
+            >
+              About
+            </Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
