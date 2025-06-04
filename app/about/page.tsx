e51a49c636eb26a7936d79bb30f198a1515ed354
Ulziiorshikh Batdorj <ulzii.batdorj@nih.gov-at-846214067917>
2025-06-04 12:01:52 -0400

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,34 @@
+export const metadata = {
+  title: "About Us - ShopNow",
+  description:
+    "Learn about ShopNow - your trusted e-commerce destination for quality products and exceptional service.",
+};
+
+export default function AboutPage() {
+  return (
+    <section className="max-w-7xl mx-auto mt-20">
+      <h1 className="text-4xl mb-6">About Us</h1>
+      <p className="mb-6">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui porro
+        accusantium omnis! Cum distinctio error molestias! Quos repellendus
+        voluptas nam eaque nisi! Veniam laudantium, nam sapiente rerum
+        repudiandae inventore nobis ipsa atque exercitationem doloribus, sunt
+        esse, quod veritatis debitis. Aliquam totam laboriosam, voluptatum vero,
+        ipsam suscipit quam error amet exercitationem at fugiat? Sunt, veniam
+        obcaecati! Quia odit consequuntur ad rerum eveniet molestias voluptate
+        aperiam porro, dignissimos beatae quas aliquid quae placeat? Itaque
+        libero doloremque nemo perferendis iste nulla magni! Dicta amet
+        voluptatem sed provident beatae, ipsam ipsum rerum nisi obcaecati
+        voluptatum consectetur praesentium quod doloremque soluta eligendi
+        recusandae officiis magni.
+      </p>
+      <div className="space-x-2">
+        <button className="btn">Look</button>
+        <button className="btn">At</button>
+        <button className="btn">These</button>
+        <button className="btn">Buttons</button>
+        <button className="btn">!</button>
+      </div>
+    </section>
+  );
+}
